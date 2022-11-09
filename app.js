590b2736bd8b0b5090e94102e82437cb30093f04
Anica Rahman <anicarahman2009@yahoo.it>
2022-11-09 16:34:46 +0000

    logo change colour done

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const bottomContainerEl = document.querySelector(".bottom-container");
 //console.log(bottomContainerEl.offsetTop);
 //console.log(navbarEl.offsetHeight);
 
+const logoEl = document.querySelector(".logo-black");
+
 window.addEventListener("scroll", () => {
   //console.log(window.scrollY);
   if (window.scrollY > bottomContainerEl.offsetTop - navbarEl.offsetHeight) {
     navbarEl.classList.add("active");
+    logoEl.setAttribute("src", "logoWhite.svg");
   } else {
     navbarEl.classList.remove("active");
+    logoEl.setAttribute("src", "logoBlack.svg");
   }
 });
